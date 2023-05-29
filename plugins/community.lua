0786918d909036c7ba0e734bde04f3299fc3bce0
Shell1010 <72198971+Shell1010@users.noreply.github.com when>
2023-05-29 12:23:16 +0100

    Updated neovim configuration

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,27 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.editing-support.auto-save-nvim"},
+  { import = "astrocommunity.media.presence-nvim"},
+  {
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        ts_rainbow2 = true,
+      },
+      transparent_background = true,
+    },
+  },
 }
